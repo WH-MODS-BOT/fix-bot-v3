4efa6513261a97e3e2cc2c927da5470619619a29
wannjb <113450108+wannjb@users.noreply.github.com>
2022-10-06 02:39:29 +0800

    Update downloader-tiktok.js

--- downloader-tiktok.js
+++ downloader-tiktok.js
@@ -1,45 +1,39 @@
-const axios = require('axios')
-const cheerio = require('cheerio')
+import fetch from 'node-fetch'
+import axios from 'axios'
+import { tiktokdl, tiktokdlv2, tiktokdlv3 } from '@bochilteam/scraper'
 
-async function TiktokDownloader (Url) {
-	return new Promise (async (resolve, reject) => {
-		await axios.request({
-			url: "https://ttdownloader.com/",
-			method: "GET",
-			headers: {
-				"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-				"accept-language": "en-US,en;q=0.9,id;q=0.8",
-				"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36",
-				"cookie": "_ga=GA1.2.1240046717.1620835673; PHPSESSID=i14curq5t8omcljj1hlle52762; popCookie=1; _gid=GA1.2.1936694796.1623913934"
-			}
-		}).then(respon => {
-			const $ = cheerio.load(respon.data)
-			const token = $('#token').attr('value')
-			axios({
-				url: "https://ttdownloader.com/req/",
-				method: "POST",
-				data: new URLSearchParams(Object.entries({url: Url, format: "", token: token})),
-				headers: {
-					"accept": "*/*",
-					"accept-language": "en-US,en;q=0.9,id;q=0.8",
-					"content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-					"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36",
-					"cookie": "_ga=GA1.2.1240046717.1620835673; PHPSESSID=i14curq5t8omcljj1hlle52762; popCookie=1; _gid=GA1.2.1936694796.1623913934"
-				}
-			}).then(res => {
-				const ch = cheerio.load(res.data)
-				const result = {
-					status: res.status,
-					result: {
-						nowatermark: ch('#results-list > div:nth-child(2)').find('div.download > a').attr('href'),
-						watermark: ch('#results-list > div:nth-child(3)').find('div.download > a').attr('href'),
-						audio: ch('#results-list > div:nth-child(4)').find(' div.download > a').attr('href')
-					}
-				}
-				resolve(result)
-			}).catch(reject)
-		}).catch(reject)
-	})
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    //try {
+if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+    const { author: { nickname }, video, description } = await tiktokdlv3(args[0])
+    .catch(async _ => await tiktokdlv2(args[0]))
+        .catch(async _ => await tiktokdl(args[0]))
+    const url = video.no_watermark || video.no_watermark2 || video.no_watermark_raw
+    if (!url) throw 'Can\'t download video!'
+    conn.sendFile(m.chat, url, 'tiktok.mp4', 
+`              *「 🇹 ᴛ ɪ ᴋ ᴛ ᴏ ᴋ 」*
+                 ████████▀▀▀████
+                 ████████────▀██
+                 ████████──█▄──█
+                 ███▀▀▀██──█████
+                 █▀──▄▄██──█████
+                 █──█████──█████
+                 █▄──▀▀▀──▄█████
+                 ███▄▄▄▄▄███████
+────────── ⇆ㅤ◁ㅤ ❚❚ㅤ ▷ㅤ↻ ──────────
+*Nickname:* ${nickname}
+*Description:* ${description}
+_©WH-MODS-BOT-V1🍭_
+`.trim(), m)
+//}
+/* catch {
+    if (!args[0]) throw 'Uhm...url nya mana?'
+  let txt = `🚀 *Link:* ${await(await axios.get(`https://tinyurl.com/api-create.php?url=${args[0]}`)).data}` 
+  conn.send2ButtonVid(m.chat, `https://api.lolhuman.xyz/api/tiktokwm?apikey=${lolkey}&url=${args[0]}`, txt, wm, `No Wm`, `.tiktoknowm ${args[0]}`, `Audio`, `.tta ${args[0]}`, m)
+    } */
 }
+handler.help = ['tiktok', 'tiktok', 'tiktokdl'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+handler.command = /^(tik(tok)?(tok)?(dl)?)$/i
 
-module.exports = { TiktokDownloader }
+export default handler
